0ce06ac8bf2d9651ea4182a7e672d46fc341453c
Daniel Hernandez Stalhuth <hsdaniele95@gmail.com>
2024-04-11 02:43:31 +0200

    kubernetes operator dag test

---
+++ dags/test_kubernetes.py
@@ -0,0 +1,27 @@
+import os
+import sys
+from datetime import datetime
+
+from airflow import DAG
+from airflow.operators.dummy import DummyOperator
+from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
+from kubernetes.client import models as k8s
+from kubernetes.client.models import V1EnvVar
+
+with DAG('test_kubernetes',
+         schedule_interval='@once',
+         start_date=datetime(2022, 1, 1),
+         catchup=False) as dag:
+
+
+    task = KubernetesPodOperator(
+        namespace='test-pod',
+        image='python:3.11',
+        cmds=["python", "-c"],
+        arguments=["print('Hello from the Kubernetes Pod!')"],
+        labels={"foo": "bar"},
+        name="airflow-test-pod",
+        task_id="task",
+        get_logs=True,
+        in_cluster=True
+    )
