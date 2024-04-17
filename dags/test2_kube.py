4eae4336e6dbd7bfab9e09c7c2005efbe7511b79
Daniel Hernandez Stalhuth <hsdaniele95@gmail.com>
2024-04-17 19:59:15 +0200

    Final test kubernetes Dags (test1 works)

--- dags/test2_kube.py
+++ dags/test2_kube.py
@@ -26,7 +26,7 @@ else:
     config_file = None
 
 with DAG(
-    dag_id="example_kubernetes_pod", schedule="@once", default_args=default_args
+    dag_id="example_kubernetes_pod", schedule="@once"
 ) as dag:
     KubernetesPodOperator(
         # config_file="/home/airflow/composer_kube_config",
@@ -35,7 +35,7 @@ with DAG(
         name="airflow-test-pod",
         task_id="task-one",
         #in_cluster=in_cluster,  # if set to true, will look in the cluster, if false, looks for file
-        cluster_context="docker-desktop",  # is ignored when in_cluster is set to True
+        # cluster_context="docker-desktop",  # is ignored when in_cluster is set to True
         #config_file=config_file,
         is_delete_operator_pod=True,
         get_logs=True,
